c0963ebf004713c4d53bbf158f17f49820c1e492
BrianKohnen <briankohnen@yahoo.com>
2021-03-18 19:52:24 -0500

    style changes. can capture mouse movements within defined plane of the 3d object, but not to desired effect

--- src/components/CubyScene/index.js
+++ src/components/CubyScene/index.js
@@ -5,9 +5,11 @@ function CubyScene() {
 
     const [slideX, setSlideX] = useState({x: -20});
     const [slideY, setSlideY] = useState({y: -20});
-    const [wheel, setWheelDir] = useState({direction: ''});
     const [audio, setAudio] = useState({src: '', open: false});
-    const [wired, setWired] = useState({bool: false});
+    const [wired, setWired] = useState({bool: false, persp: false});
+    const [mDown, setmDown] = useState({down: false});
+    const [cliX, setcliX] = useState({x: -20});
+    const [cliY, setcliY] = useState({y: -20});
 
     const handleXSlide = (e) => {
         setSlideX({x: e.target.value});
@@ -33,14 +35,6 @@ function CubyScene() {
         };
     };
 
-    const handleScroll = (e) => {
-        if (e.deltaY < 0) {
-            setWheelDir({direction: -0.5});
-        } else if (e.deltaY > 0) {
-            setWheelDir({direction: 0.5});
-        }
-    };
-
     const openInNewTab = (url) => {
         const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
         if (newWindow) newWindow.opener = null;
@@ -50,17 +44,54 @@ function CubyScene() {
         setWired({bool: !wired.bool});
     };
 
-    useEffect(() => {
-        window.addEventListener('wheel', handleScroll);
-        return () => window.removeEventListener('wheel', handleScroll);
-    }, []);
+    const handleWiredHov = (hovbool) => {
+        setWired({bool: wired.bool, hovered: hovbool, persp: wired.persp});
+    };
+
+    const setPerspective = () => {
+        setWired({bool: wired.bool, persp: !wired.persp});
+    };
+
+    // const handleMousedown = (e) => {
+    //     setmDown({down: true});
+    //     console.log(e.target.getBoundingClientRect(), e.target.offsetLeft);
+    //     //x: 450 750 1100, 50 300 550 y
+    // };
+
+    // const handleMouseup = (e) => {
+    //     setmDown({down: false});
+    // };
+
+    // let planeLeft = (441 + 654) / 2;
+    // let planeTop = (76.8 + 504) / 2;
+
+    // const handleMousemove = (e) => {
+    //     if (mDown.down) {
+    //         console.log(planeLeft, e.clientX - planeLeft, e.clientY - planeTop);
+    //         setcliX({x: (e.clientX - planeLeft)});
+    //         setcliY({y:  -(e.clientY - planeTop) / 10});
+    //         // planeLeft = e.clientX - planeLeft;
+    //         // planeTop = e.clientY - planeTop;
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     window.addEventListener('mousemove', handleMousemove);
+    //     return (() => window.removeEventListener('mousemove', handleMousemove));
+    // }, []);
 
     
 
   return (
       <section className='threeDWrap' style={wired.bool ? {background:'black'} : {}}>
 
-            <div className='wiredHolder' onClick={handleWired} style={wired.bool ? {color: 'yellow'} : {}}>
+            <div className='wiredHolder' onMouseOver={()=>handleWiredHov(true)} onMouseOut={()=>handleWiredHov(false)} onClick={handleWired} style={wired.bool ? 
+                {background: "linear-gradient(to bottom, rgb(209, 152, 152) 50%, yellow 50%) 100%", 
+                backgroundSize: '100% 200%', 
+                backgroundPosition: `${wired.hovered ? 'top' : 'bottom'}`, 
+                color: 'transparent', 
+                WebkitBackgroundClip: 'text'} 
+                : {}}>
                 O
             </div>
 
@@ -78,7 +109,7 @@ function CubyScene() {
 
         <div className={`audioStuffs ${audio.open ? '' : 'hidestuffs'}`}>
             <input id='audioFile' type='file' name='file' accept='audio/*' className='audioFile' onChange={handleAudioInput} />
-            <label for='audioFile' style={wired.bool ? {background: 'yellow', color: 'black'} : {}}>upload an audio file</label>
+            <label htmlFor='audioFile' style={wired.bool ? {background: 'yellow', color: 'black'} : {}}>upload an audio file</label>
 
             <audio controls src={audio.src} type='audio/mp3' autoPlay>
             </audio>
@@ -87,8 +118,10 @@ function CubyScene() {
         <div className='backText' style={wired.bool ? {zIndex: '50'} : {}}>
             <h1 style={wired.bool ? {color: 'white'} : {}}>A MATTER</h1>
             <h1 style={wired.bool ? {color: 'white'} : {}}>OF</h1>
-            <h1 style={wired.bool ? {color: 'yellow'} : {}}>PERSPECTIVE</h1>
+            <h1 style={wired.bool ? {color: 'yellow'} : {}} onClick={setPerspective}>PERSPECTIVE</h1>
         </div>
+
+        {/* <div className='mouseTrack' onMouseMove={handleMousemove} onMouseDown={handleMousedown} onMouseUp={handleMouseup}></div> */}
 
         <div className='scene' style={{transform: `rotateX(${slideY.y}deg) rotateY(${slideX.x}deg) scale(1)`}}>
             <div className='cuboid fullC' style={ wired.bool ? {border: '1px solid rgb(209, 152, 152)', background:'transparent'} : {}}>
